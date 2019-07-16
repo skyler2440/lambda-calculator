9e1d421118a8f933217b6603bc45553446074a6e
skyler dowdy <skylerwebdev@gmail.com>
2019-07-16 15:33:14 -0400

    operators display specials

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Specials from "./Specials";
 const SpecialButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button elemen rendering the data being passed down from the parent container on props */}
+      <Specials />
     </>
   );
 };
+export default SpecialButton
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import { specials } from "../../../data";
+import "./Specials.scss"
 
 //import any components needed
 
@@ -6,12 +8,14 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+useState({specials})
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+    {specials.map (item => <button className="specials">{item}</button>)}
     </div>
   );
 };
+export default Specials;
